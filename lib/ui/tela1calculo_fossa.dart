508856976b1fd7ca35003f33ca8dc545654d65cc
FelippeNO <felippenegrao@alunos.utfpr.edu.br>
2022-01-15 13:45:25 -0300

    16

--- lib/ui/tela1calculo_fossa.dart
+++ lib/ui/tela1calculo_fossa.dart
@@ -2,23 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:fossa_septica/main.dart';
 import 'package:fossa_septica/ui/tela2.dart';
 
-
 class Tela1CalculoFossa extends StatefulWidget {
-  const Tela1CalculoFossa({ Key? key }) : super(key: key);
+  const Tela1CalculoFossa({Key? key}) : super(key: key);
 
   @override
   _Tela1CalculoFossaState createState() => _Tela1CalculoFossaState();
 }
 
 class _Tela1CalculoFossaState extends State<Tela1CalculoFossa> {
-@override
-  final TextEditingController controleQntd = new TextEditingController();
+  @override
+  final TextEditingController controleQntd = TextEditingController();
 
   List<DropdownMenuItem<String>> get dropdownItems {
-    controleQntd.clear();
+    //  controleQntd.clear();
     List<DropdownMenuItem<String>> menuItems = [
-      DropdownMenuItem(child: Text("Clique para selecionar"),value: "Clique para selecionar"),
-      DropdownMenuItem(child: Text("Permanente",), value: "Permanente"),
+      DropdownMenuItem(
+          child: Text("Clique para selecionar"),
+          value: "Clique para selecionar"),
+      DropdownMenuItem(
+          child: Text(
+            "Permanente",
+          ),
+          value: "Permanente"),
       DropdownMenuItem(child: Text("Temporária"), value: "Temporária"),
     ];
     return menuItems;
@@ -201,7 +206,8 @@ class _Tela1CalculoFossaState extends State<Tela1CalculoFossa> {
               color: Color.fromRGBO(112, 112, 122, 1),
               fontSize: 18.0,
               fontWeight: FontWeight.bold),
-        ),),
+        ),
+      ),
       body: Center(
         child: Container(
           decoration: BoxDecoration(
@@ -218,19 +224,53 @@ class _Tela1CalculoFossaState extends State<Tela1CalculoFossa> {
               Column(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text(
-                      "Selecione o Tipo da Ocupação:",
-                      style: TextStyle(fontSize: 16.0),
-                    ),
+                    padding: EdgeInsets.all(20),
+                    child: Container(
+                        //inicio primeiro texto com fundo laranja
+                        height: 40,
+                        width: 220,
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(
+                                255, 191, 64, 1), //laranja padrao
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25))),
+                        child: Center(
+                          child: Text(
+                            "Selecione o tipo da Ocupação:",
+                            style: TextStyle(
+                                color: Color.fromRGBO(
+                                    64, 139, 255, 1), // azul padrao
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15.0),
+                            textAlign: TextAlign.center,
+                          ),
+                        )),
                   ),
                   Padding(
+                    // COMEÇA AQUI O PRIMEIRO DROPDOWN
                     padding: const EdgeInsets.all(8.0),
                     child: Container(
-                      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(5.0)),),
+                      alignment: Alignment.center,
+                      constraints: BoxConstraints.tight(Size(250, 50)),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black,
+                              spreadRadius: 0.00,
+                              blurRadius: 3.0)
+                        ],
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: const [
+                              Color.fromRGBO(89, 156, 255, 1), //azul escuro
+                              Color.fromRGBO(64, 139, 255, 1), //azul padrao
+                            ]),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      ),
                       child: DropdownButton(
+                        underline: SizedBox(),
                         dropdownColor: Colors.white,
-                        iconDisabledColor: Colors.green,
                         iconEnabledColor: Colors.orange,
                         focusColor: Colors.orange,
                         borderRadius: BorderRadius.all(Radius.circular(10)),
@@ -245,55 +285,130 @@ class _Tela1CalculoFossaState extends State<Tela1CalculoFossa> {
                         items: dropdownItems,
                       ),
                     ),
+                  ), // TERMINA AQUI O PRIMEIRO DROPDOWN
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Container(
+                        //inicio segundo texo fundo laranja
+                        height: 40,
+                        width: 220,
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(
+                                255, 191, 64, 1), //laranja padrao
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25))),
+                        child: Center(
+                          child: Text(
+                            "Selecione o tipo do Prédio:",
+                            style: TextStyle(
+                                color: Color.fromRGBO(
+                                    64, 139, 255, 1), // azul padrao
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15.0),
+                            textAlign: TextAlign.center,
+                          ),
+                        )),
                   ),
                   Padding(
+                    // COMEÇA AQUI O SEGUNDO DROPDOWN
                     padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Selecione o Tipo do Prédio:",
-                      style: TextStyle(fontSize: 16.0),
+                    child: Container(
+                      alignment: Alignment.center,
+                      constraints: BoxConstraints.tight(Size(330, 50)),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black,
+                              spreadRadius: 0.00,
+                              blurRadius: 3.0)
+                        ],
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: const [
+                              Color.fromRGBO(89, 156, 255, 1), //azul escuro
+                              Color.fromRGBO(64, 139, 255, 1), //azul padrao
+                            ]),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      ),
+                      child: DropdownButton(
+                        underline: SizedBox(),
+                        dropdownColor: Colors.white,
+                        iconEnabledColor: Colors.orange,
+                        focusColor: Colors.orange,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        itemHeight: 60,
+                        value: valor2_selecionado,
+                        onChanged:
+                            valor1_selecionado != "Clique para selecionar"
+                                ? (String? newValue) {
+                                    setState(() {
+                                      valor2_selecionado = newValue!;
+                                      controleQntd.clear();
+                                    });
+                                  }
+                                : null,
+                        items: dropdownItems2,
+                      ),
                     ),
+                  ), //SEGUNDO DROPDOWN FIM
+                  Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Container(
+                        //inicio segundo texo fundo laranja
+                        height: 40,
+                        width: 250,
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(
+                                255, 191, 64, 1), //laranja padrao
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25))),
+                        child: Center(
+                          child: Text(
+                            "Digite a quantidade de $tipagem:",
+                            style: TextStyle(
+                                color: Color.fromRGBO(
+                                    64, 139, 255, 1), // azul padrao
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15.0),
+                            textAlign: TextAlign.center,
+                          ),
+                        )),
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: DropdownButton(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      itemHeight: 80,
-                      value: valor2_selecionado,
-                      onChanged: valor1_selecionado != "Clique para selecionar"
-                          ? (String? newValue) {
-                              setState(() {
-                                valor2_selecionado = newValue!;
-                                controleQntd.clear();
-                              });
-                            }
-                          : null,
-                      items: dropdownItems2,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Digite a quantidade de $tipagem:",
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: (TextField(
+                    padding: const EdgeInsets.only(
+                        left: 100.0, right: 100.0, top: 15.0, bottom: 15.0),
+                    child: (TextFormField(
+                      textAlign: TextAlign.center,
                       enabled: valor2_selecionado != "Clique para selecionar"
                           ? true
                           : false,
                       keyboardType: TextInputType.number,
                       cursorColor: Colors.blue,
-                      style: TextStyle(color: Colors.blue, fontSize: 12),
+                      style: TextStyle(
+                          color: Color.fromRGBO(255, 191, 64, 1),
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
                       controller: controleQntd,
                       decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 2.5)),
+                        disabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color.fromRGBO(120, 120, 120, 0.5),
+                                width: 3.0)),
                         focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue, width: 3.0)),
-                        labelStyle: TextStyle(color: Colors.blue),
-                        hintText: "",
+                            borderSide: BorderSide(
+                                color: Color.fromRGBO(255, 191, 64, 1),
+                                width: 3.0)),
+                        labelStyle:
+                            TextStyle(color: Color.fromRGBO(255, 191, 64, 1)),
+                        hintText: "Ex.: 5",
                         border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue, width: 1.5),
+                            borderSide: BorderSide(
+                                color: Color.fromRGBO(255, 191, 64, 1),
+                                width: 2),
                             borderRadius:
                                 const BorderRadius.all(Radius.circular(20.0))),
                       ),
@@ -301,8 +416,8 @@ class _Tela1CalculoFossaState extends State<Tela1CalculoFossa> {
                   ),
                   ElevatedButton(
                     style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.blue),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromRGBO(255, 191, 64, 1)),
                     ),
                     child: Text("Avançar >"),
                     onPressed: () => AvancarTela2(controleQntd),
